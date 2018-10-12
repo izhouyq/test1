95e5033304480f9fcb403c27d6a802ae69359eb4
zhouyq <zhouyq@carnation.com.cn>
2018-10-12 10:32:46 +0800

    sssss

--- src/src/main/java/com/carnation/demo/DemoApplication.java
+++ src/src/main/java/com/carnation/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
     public static void main(String[] args) {
     	   System.out.println("zhouyq");
+    	   System.out.println("zhouyq");
         System.out.println("demoTest123Test");
         SpringApplication.run(DemoApplication.class, args);
     }
