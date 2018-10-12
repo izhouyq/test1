948cb81c8a3787ee086851988a5f4d4ee1d31e5c
zhouyq <zhouyq@carnation.com.cn>
2018-10-12 10:50:26 +0800

    奔奔要

--- src/src/main/java/com/carnation/demo/DemoApplication.java
+++ src/src/main/java/com/carnation/demo/DemoApplication.java
@@ -7,6 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+    	   System.out.println("zhouyqaaaaaaaaaaaaa");
+    	   System.out.println("bbbbbbbbbbbb");
+    	   System.out.println("bbbbbbbbbbbb");
+    	   System.out.println("zhouyq1");
+    	   System.out.println("zhouyq2");
+        System.out.println("demoTest123Test");
         SpringApplication.run(DemoApplication.class, args);
     }
 }
