c210a6ef41903711a47432f4ffd8214b0a54c521
zhouyq <zhouyq@carnation.com.cn>
2018-10-12 10:52:25 +0800

    ssf

--- src/src/main/java/com/carnation/demo/DemoApplication.java
+++ src/src/main/java/com/carnation/demo/DemoApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
+    	System.out.println("zhouyqaaaaaaaaaaaaa");
     	   System.out.println("zhouyqaaaaaaaaaaaaa");
     	   System.out.println("bbbbbbbbbbbb");
     	   System.out.println("bbbbbbbbbbbbs");
